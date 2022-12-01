133bc95feaf91ed620bdb6431547e6e7a603c1d0
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-12-01 18:45:06 -0300

    Resolucao-cond2-exercicio

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,25 @@
-console.log("teste 1")
+const numeroRecebido = +(prompt("Digite seu melhor número!"))
+if (numeroRecebido%2===0){
+    if (numeroRecebido%3===0){
+        console.log(`${numeroRecebido} É divisivel por 2 e 3!`)
+    }else{
+        console.log(`${numeroRecebido} Divide por 2 mas não por 3`)
+    }
+}else {
+    console.log(`${numero} Não se divide por 2 e por 3!`)
+}
+        
+console.log("Número recebido" , numeroRecebido)
+console.log("Divisão por 2", numeroRecebido%2===0)
+console.log("Divisão por 3", numeroRecebido%3===0)
 
+
+//Operador logico
+
+const numeroRecebido1 = +(prompt("Digite seu melhor número!"))
+
+if(numeroRecebido1%2===0 && numeroRecebido1%3===0){
+    console.log(`${numeroRecebido1} Divide por 2 e por 3!`)
+}else {
+    console.log(`${numeroRecebido1} Não divide por 2 e por 3!`)
+}
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,17 +1,24 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+let nacionalidade = prompt("Digite aqui a nacionalidade").toLowerCase().trim();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
+function naci (){
+    switch (nacionalidade){
+    case "brasileira":
+        console.log("brasileira")
+        break;
+    case "argentina":
+        console.log("argentina")
+        break;
+    case "uruguaia":
+        console.log("uruguaia")
+        break;
+    case "chilena":
+        console.log("chilena")
+        break;
+    case "colombiana":
+        console.log("colombiana")
+        break;
+    default:
+        console.log(`Nacionalidade não encontrada. Tu és ${nacionalidade}`)
+    }
 }
-
-console.log("teste 2")
+naci ()
